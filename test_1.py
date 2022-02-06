5f2df47306e2c4aee57278a413602adb60eced9d
RiccardoPaladin <98117928+RiccardoPaladin@users.noreply.github.com>
2022-02-06 11:34:39 -0500

    Delete test_1.py

--- test_1.py
+++
@@ -1,8 +0,0 @@
-import requests
-recr  = requests.get("http://www.spacejam.com/1996")
-recr.content
-recr.status_code   #it has to show 200
-
-
-
-requests.post()     #post is another kind of request
