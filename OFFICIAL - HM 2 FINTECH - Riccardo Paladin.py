b6e0e7bb992fe46c46282afd59c0918437b55d8b
RiccardoPaladin <rpaladin@stevens.edu>
2022-03-04 17:36:29 -0500

    HM 2 - Reviewed with functions

--- OFFICIAL - HM 2 FINTECH - Riccardo Paladin.py
+++ OFFICIAL - HM 2 FINTECH - Riccardo Paladin.py
@@ -10,42 +10,55 @@ import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
 
-webscraping = []
-lines = []
+def obtain_web_info(url):
+    '''
+    Function to obtain infomation from the website
+    :input: URL
+    '''
+    webscraping = []
+    lines = []
+    for _ in range(50):
+        rec = requests.get(url)
+        if rec.status_code == 200:
+            soup = BeautifulSoup(rec.text, 'html.parser')
+            for body in soup.find_all('body'):
+                c = body.get_text()
+                lines = c.split('\n')
+                webscraping.append(lines)
 
-#For loop to list the body of the website
-for _ in range(50):
-    url = 'http://34.238.119.208:8000/random_company'
-    rec = requests.get(url)
-    if rec.status_code == 200:
-        soup = BeautifulSoup(rec.text, 'html.parser')
-        for body in soup.find_all('body'):
-            c = body.get_text()
-            lines = c.split('\n')
-            webscraping.append(lines)
-    else:
-        print('Error:', rec.status_code)
+        else:
+            print('Error:', rec.status_code)
+            break
+    return webscraping
 
-names = []
-purposes = []
+url = 'http://34.238.119.208:8000/random_company'
+webscraping = obtain_web_info(url)
 
-#For loop to create lists for names and purposes of the companies
-for i in range(50):
-    for y in webscraping[i]:
-        if 'Name:' in y:
-            names.append(y)
-    for x in webscraping[i]:
-        if 'Purpose:' in x:
-            purposes.append(x)
+def name_purpose(webscraping):
+    '''
+    Function to organize in a dataset with names and purposes of the companies
+    :input: information obtained from the webscraping
+    '''
+    names = []
+    purposes = []
+    dataset = pd.DataFrame()
+    for i in range(50):
+        for y in webscraping[i]:
+            if 'Name:' in y:
+                names.append(y)
+        for x in webscraping[i]:
+            if 'Purpose:' in x:
+                purposes.append(x)
+    df_names = pd.DataFrame(names)
+    df_purposes = pd.DataFrame(purposes)
+    dataset = pd.concat([df_names, df_purposes], axis = 1)
+    dataset.columns = ['Names', 'Purposes']
+    dataset['Names'] = dataset['Names'].str.replace('Name:', '')
+    dataset['Purposes'] = dataset['Purposes'].str.replace('Purpose:', '')
+    return dataset
 
-#Organize results in dataset
-df_names = pd.DataFrame(names)
-df_purposes = pd.DataFrame(purposes)
-dataset = pd.concat([df_names, df_purposes], axis = 1)
-dataset.columns = ['Names', 'Purposes']
-dataset['Names'] = dataset['Names'].str.replace('Name:', '')
-dataset['Purposes'] = dataset['Purposes'].str.replace('Purpose:', '')
-print(dataset)
+dataset = name_purpose(webscraping)
+dataset
 
 #Save and store dataset
 dataset.to_csv('webscrape_companies.csv')
